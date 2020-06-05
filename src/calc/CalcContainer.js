70d186732ec09c49d94ca56f72a6ab16524c6e57
ChoSeungHei <kwa0403261@gmail.com>
2020-06-05 17:21:08 +0900

    fix useState

--- src/calc/CalcContainer.js
+++ src/calc/CalcContainer.js
@@ -3,22 +3,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../style/css/font.css';
 
 const CalcContainer = () => {
-  const [aActive,setA] = useState(true);
-  const [mActive,setM] = useState(false);
+  const [cate,setCate] = useState('annual');
+  const [salary,setSalary] = useState('');
 
-  const switchAnM = (value,e) => {
-    if(value==='a')
-    {
-      setA(true);
-      setM(false);
-    }
-    else
-    {
-      setM(true);
-      setA(false);
-    }
-    console.log(value);
+  const switchAnM = (e) => {
+    setCate(e.target.value);
+    console.log(cate);
   }
+
+  const handleSalary = (e) => {
+    const re = /^[0-9\b]+$/;
+
+    if (e.target.value == '' || re.test(e.target.value)) {
+      var temp = e.target.value;
+      temp = temp.toLocaleString('en');
+      setSalary(temp);
+    }
+    
+    console.log(salary);
+
+  }
+
 
   return (
 
@@ -31,16 +36,19 @@ const CalcContainer = () => {
       <div className="row">
       <div className="col col-4 text-center">
       <div className="btn-group btn-group-toggle " data-toggle="buttons">
-        <label className={`btn btn-secondary ${aActive ? "active" : ""}`}>
-          <input type="radio" name="options" id="option1" onClick={(e)=> switchAnM('a',e)}/> 연봉
+        <label className={`btn btn-secondary ${cate === 'annual' ? "active" : ""}`}>
+          <input type="radio" value="annual" onClick={(e)=> switchAnM(e)}/> 연봉
         </label>
-        <label className={`btn btn-secondary ${mActive ? "active" : ""}`}>
-          <input type="radio" name="options" id="option2" onClick={(e)=> switchAnM('m',e)}/> 월급
+        <label className={`btn btn-secondary ${cate === 'monthly' ? "active" : ""}`}>
+          <input type="radio" value="monthly" onClick={(e)=> switchAnM(e)}/> 월급
         </label>
       </div>
       </div>
       <div className="col col-8 text-center">
-        <input type="text" className="form-control" placeholder="Text input"/>
+        <input type="text" className="form-control" placeholder="Text input"
+          value={salary}
+          onChange={handleSalary}
+        />
       </div>
       </div>
     </div>
